caff6abd5cdf3f46b9c8d2d89fa159b19e6fd8a9
Piet Bos <info@senlinonline.com>
2014-08-17 17:08:40 +0800

    update structure

--- config.rb
+++ config.rb
@@ -3,13 +3,14 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
