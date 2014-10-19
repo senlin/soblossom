4952ad7d31c1c91bc8740a6967eb7d45700eab20
Piet Bos <info@senlinonline.com>
2014-10-19 13:07:00 +0800

    remove unnecessary comparison

--- single.php
+++ single.php
@@ -20,7 +20,7 @@ get_header(); ?>
 					soblossom_post_nav(); // defined in inc/soblossom.php
 		
 					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) {
+					if ( comments_open() || get_comments_number() ) {
 						comments_template();
 					} //endif
 		
