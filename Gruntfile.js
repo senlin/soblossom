928eacf46cc651d427111bcef1f6b4941399caaa
Piet Bos <info@senlinonline.com>
2015-02-05 16:30:03 +0800

    switch to Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
+        options: {
+          // outputStyle: 'compressed'
+          outputStyle: 'expanded'
+        },
+        files: {
+          'css/style.css': 'scss/style.scss',
+          'css/custom-login.css': 'scss/custom-login.scss'
+        }        
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['build','watch']);
+}
