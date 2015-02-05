17935c0b1c25647e009d5c4ee003d9f603276785
Piet Bos <info@senlinonline.com>
2015-02-05 15:28:53 +0800

    Enable close clearing by clicking anywhere on mask

--- js/soblossom.js
+++ js/soblossom.js
@@ -15,5 +15,20 @@
 	// Add button class to all submit buttons
 	//$('input[type="submit"]').addClass('button');
 
+	/**
+	 * Enable closing clearing (lightbox) by clicking anywhere on the mask
+	 *
+	 * @source: //github.com/zurb/foundation/issues/4119 answer of @javierarques
+	 */
+	$(document).on('click', '.clearing-blackout', function () {
+		var container = $('.clearing-container');
+		var visible_image = container.find('.visible-img');
+		container.find('ul[data-clearing]')
+		.attr('style', '').closest('.clearing-blackout')
+		.removeClass('clearing-blackout');
+		container.removeClass('clearing-container');
+		visible_image.fadeOut();
+	});	
+
 
 }(jQuery)); 
