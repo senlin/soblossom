2606ea864dc96070af3a9d5ffc366d1b92033338
Piet Bos <info@senlinonline.com>
2015-02-08 00:32:41 +0800

    sample Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,99 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-    sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
-      dist: {
-        options: {
-          // outputStyle: 'compressed'
-          outputStyle: 'expanded'
+		sass: {
+			options: {
+				includePaths: ['bower_components/foundation/scss']
+			},
+	      
+			dev: {
+				options: {
+					outputStyle: 'expanded'
+				},
+		        files: {
+		          'css/style.css': 'scss/style.scss'
+		        }        
+			},
+	
+			release: {
+				options: {
+					outputStyle: 'compressed'
+				},
+				files: {
+					'css/style.min.css': 'scss/style.scss'
+				}        
+			}
+		},
+	
+	    watch: {
+			grunt: {
+				files: ['Gruntfile.js']
+			},
+	
+			sass: {
+				files: 'scss/**/*.scss',
+				tasks: 'sass:dev'
+			},
+	      
+			php: {
+				files: ['*.php', 'inc/{,*/}*.php', 'templates/{,*/}*.php', 'tplparts/{,*/}*.php'],
+			},
+
+		    options: {
+		        livereload: true,
+		        spawn: false
+		    }
+	    },
+
+        concat: {
+            dev: {
+	            src: [
+		           'bower_components/modernizr/modernizr.js',
+		           'bower_components/foundation/js/foundation.js',
+		           'js/soblossom.js',
+		           'js/skip-link-focus-fix.js'
+	            ],
+	            dest: 'js/combined.js',
+            },
         },
-        files: {
-          'css/style.css': 'scss/style.scss',
-          'css/custom-login.css': 'scss/custom-login.scss'
-        }        
-      }
-    },
 
-    watch: {
-      grunt: { files: ['Gruntfile.js'] },
+		uglify: {
+		    release: {
+		        src: 'js/combined.js',
+		        dest: 'js/combined.min.js'
+		    }
+		},
 
-      sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
-      }
-    }
-  });
+    	// https://www.npmjs.org/package/grunt-wp-i18n
+	    makepot: {
+	        target: {
+	            options: {
+	                domainPath: '/languages/',    // Where to save the POT file.
+	                potFilename: 'soblossom.pot',   // Name of the POT file.
+	                type: 'wp-theme'  // Type of project (wp-plugin or wp-theme).
+	            }
+	        }
+	    }
+	    
+	});
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-wp-i18n');
+	
+	grunt.registerTask( 'default', [
+		'sass:dev',
+		'concat',
+		'watch'
+	]);
 
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
+	grunt.registerTask( 'release', [
+		'sass:release',
+		'concat',
+		'uglify:release',
+		'makepot'
+	]);
 }
